62e978e1792cc8f8cfbec0d4dbbe5908ca663310
aaron-edwards <aedwards@thoughtworks.com>
2017-01-13 14:30:06 +1100

    Split clustering out to a package

---
+++ analyse/cluster.go
@@ -0,0 +1,70 @@
+package analyse
+
+import(
+  "image"
+  "time"
+  "log"
+	"github.com/mdesenfants/gokmeans"
+	"colour-extractor/img-process"
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+func toNode(data [][3]float64) ([]gokmeans.Node) {
+	var nodes []gokmeans.Node
+	for x := 0; x < len(data); x++ {
+		nodes = append(nodes, gokmeans.Node{data[x][0], data[x][1], data[x][2]})
+	}
+
+	return nodes
+}
+
+func toHex(col gokmeans.Node) string {
+  return colorful.Hsl(col[0], col[1], col[2]).Hex()
+}
+
+type HSL struct {
+  H float64
+  S float64
+  L float64
+}
+
+type ClusterData struct {
+  CenterRGB string
+  CenterHSL HSL
+  PixelRatio float64
+}
+
+func groupNodes(centroids []gokmeans.Node, data []gokmeans.Node) ([]ClusterData) {
+  var colourGroups = make(map[int][]gokmeans.Node)
+  for _, node := range data {
+    index := gokmeans.Nearest(node, centroids)
+    colourGroups[index] = append(colourGroups[index], node)
+  }
+
+  var clusters  []ClusterData
+  for index, group := range colourGroups {
+    centroid := centroids[index]
+    hsl := HSL{ H: centroid[0], S: centroid[1], L: centroid[2]}
+    rgb := toHex(centroid)
+    ratio := float64(len(group)) / float64(len(data))
+    clusters = append(clusters, ClusterData{ CenterRGB: rgb, CenterHSL: hsl, PixelRatio: ratio })
+  }
+
+  return clusters
+}
+
+func Cluster(img image.Image) ([]ClusterData) {
+	pixels := imgProcess.GetPixels(img, 0.75)
+	nodes := toNode(pixels)
+
+  start := time.Now()
+  k := 5
+	_, centroids := gokmeans.Train(nodes, k, 20)
+  log.Printf("time to cluster:\t%s", time.Since(start))
+
+  groups := groupNodes(centroids, nodes)
+
+  return groups
+}
+
+
--- routes/analyse.go
+++ routes/analyse.go
@@ -1,26 +1,15 @@
 package routes
 
 import (
-	"log"
-	"time"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"colour-extractor/img-process"
-	"github.com/mdesenfants/gokmeans"
-	"github.com/lucasb-eyer/go-colorful"
+	"colour-extractor/analyse"
 	"image"
 	_ "image/png"
 	_ "image/jpeg"
 )
 
-func toNode(data [][3]float64) ([]gokmeans.Node) {
-	var nodes []gokmeans.Node
-	for x := 0; x < len(data); x++ {
-		nodes = append(nodes, gokmeans.Node{data[x][0], data[x][1], data[x][2]})
-	}
-
-	return nodes
-}
 
 func GetAnalyse(c *gin.Context) {
 	imageUrl := c.Query("imageUrl")
@@ -29,8 +18,6 @@ func GetAnalyse(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{ "error": "missing 'imageUrl' query param" })
 		return
 	}
-
-	start := time.Now()
 
 	response, err := http.Get(imageUrl)
 	if err != nil {
@@ -44,41 +31,10 @@ func GetAnalyse(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{ "error": err })
 	}
 
-	log.Printf("Downloading took %s", time.Since(start))
-  downloadTime := time.Now()
+	img = imgProcess.ResizeImage(img, 100 * 100)
+  centroids := analyse.Cluster(img)
 
-	img = imgProcess.ResizeImage(img, 50 * 50)
-	pixels := imgProcess.GetPixels(img, 0.75)
-	nodes := toNode(pixels)
-
-	log.Printf("Processing took %s", time.Since(downloadTime))
-  imgProcessTime := time.Now()
-
-  k := 5
-	_, centroids := gokmeans.Train(nodes, k, 20)
-
-	log.Printf("Clustering took %s", time.Since(imgProcessTime))
-  clusterTime := time.Now()
-
-
-  var colourGroups = make(map[int]int)
-  for _, node := range nodes {
-    index := gokmeans.Nearest(node, centroids)
-    colourGroups[index] ++
-  }
-
-  var colours = make(map[string]float64)
-  for index, centroid := range centroids {
-    colours[toHex(centroid)] = float64(colourGroups[index]) / float64(len(nodes))
-  }
-
-
-	log.Printf("Grouping took %s", time.Since(clusterTime))
-
-	c.JSON(http.StatusOK, gin.H{ "imageUrl": imageUrl, "colours": colours})
+	c.JSON(http.StatusOK, gin.H{ "centroids": centroids })
 }
 
-func toHex(col gokmeans.Node) string {
-  return colorful.Hsl(col[0], col[1], col[2]).Hex()
-}
 
